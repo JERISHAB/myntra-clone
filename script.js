8be2f9251e511b89317df29b804e9856b99a6c13
JERISHAB <jerishabmayan@gmail.com>
2025-07-09 21:18:41 +0530

    feat: implemented ui of fiters

--- script.js
+++ script.js
@@ -8,20 +8,21 @@ async function fetchProducts() {
       products.products.forEach((product) => {
       html += `
           <div class="product-card">
-            <div style="position: relative;">
+            <div class="prod-img-div">
               <img src="${product.productImage}" alt="${product.title}">
-            </div>
-            <div class="rating-box">
+              <span class="rating-box">
                 <span class="rating">${product.rating}</span>
-                <div  class="star-icon"></div>
+                <span  class="star-icon"></span>
                 <span class="count">| ${product.noOfRatings}</span>
+              </span>
             </div>
+            
             <div class="product-info">
               <div class="product-title">${product.title}</div>
               <div class="product-desc">${product.description}</div>
               <div>
-                <span class="price">₹${product.discountPrice}</span>
-                <span class="original-price">Rs ${product.originalPrice}</span>
+                <span class="price">Rs. ${product.discountPrice}</span>
+                <span class="original-price">Rs. ${product.originalPrice}</span>
                 <span class="discount">(${product.discountPercentage}% OFF)</span>
               </div>
             </div>
